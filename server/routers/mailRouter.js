c9ca0aef5082b3ec7fd93fe7aedac8c2726d5bfb
jonasegehrke <jonasegehrke@gmail.com>
2022-04-05 17:48:37 +0200

    created a small note for my teacher anders, reasoning why i didnt create forgot password & thank you for your purchase emial

--- server/routers/mailRouter.js
+++ server/routers/mailRouter.js
@@ -3,6 +3,11 @@ import nodemailer from 'nodemailer';
 
 const router = Router();
 
+
+// OBS: i did not create a function to send mail through "forgot password" & "thank you for your purchase" messages, 
+// The reason why is that it would require the user to enter his email and his password to the email. I dont want to do that (security matters).
+// If it is possible to do without requiring password to the email, i would love to know.
+
 router.post("/api/mail" , (req, res) =>{
     console.log("mail >>> ", req.body)
     const transporter = nodemailer.createTransport({
